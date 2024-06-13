fc48cef2b3c0c4d70cfd3d1149114d9b1e22dffd
khinezarthwe <khinezar70122107@gmail.com>
2024-06-13 11:12:18 +0900

    getparams and getone

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-const express = require('express');//it returns fun
+const express = require("express"); //it returns fun
 
-const app = express();//it represents application
+const app = express(); //it represents application
 
-app.get('/',(req,res)=>{
-   res.send('Hello World');
-})
+const courses = [
+    {id : 1, name: 'course1'},
+    {id : 2, name: 'course2'},
+    {id : 3, name: 'course3'}
+  ]
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 //it takes two argument ,path and second argument is callback fun
 //callback fun has two arguments -> request and response
 
-app.get('/api/courses',(req,res)=>{
-   res.send([1,2,3,4]);
-})
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3, 4]);
+});
 
-app.listen(3000,()=>console.log('Listening on port 3000'));
+//for single course
+app.get("/api/course/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+    res.send(req.params);
+  });
+
+  
+
+  app.get("/api/posts/:year/:month", (req, res) => {
+    res.send(req.params);
+  });
+  
+
+//Port
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port 3000 ${port}`));
 // app.post()
 
 // app.put()
