837a6efe3905596f3238c1a204e89d032e259eb4
khinezarthwe <khinezar70122107@gmail.com>
2024-07-06 12:54:28 +0900

    ui

--- SequelizeLessons/router/userRouter.js
+++ SequelizeLessons/router/userRouter.js
@@ -2,9 +2,9 @@ const userRouter = require("express").Router();
 const userController = require("../controller/userController");
 
 userRouter.post("/addUser",userController.addNewUser);
-// userRouter.get("/getAllUser",userController.getAllUser);
-// userRouter.get("/getUser/:id",userController.getUser);
-// userRouter.put("/updateUser/:id",userController.updateUser);
-// userRouter.delete("/deleteUser/:id",userController.deleteUser);
+userRouter.get("/getAllUser",userController.getAllUser);
+userRouter.get("/getUser/:id",userController.getUser);
+userRouter.put("/updateUser/:id",userController.updateUser);
+userRouter.delete("/deleteUser/:id",userController.deleteUser);
 
 module.exports = userRouter;
